350d9657858573025c59105cb74021a4ec4163bf
theodores <theodoresibp@gmail.com>
2023-11-24 10:01:54 +0700

    chore:add eslint rules

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-  return <div className="border-1 bg-rose-700 font-bold text-red-400 " />;
+  return <div className="w-full bg-red-500 p-8 text-center">TEST</div>;
 }
